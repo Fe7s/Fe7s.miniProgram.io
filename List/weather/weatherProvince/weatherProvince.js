df01c323685edc0e61ac0b9e71d58326031836a6
Dove <hejunqiang2014@outlook.com>
2019-03-12 16:52:52 +0800

    CaitlynMarket
    加入了tabbar，重新调整了项目的架构，加入CaitlynMarket页面

---
+++ List/weather/weatherProvince/weatherProvince.js
@@ -0,0 +1,79 @@
+// pages/weatherProvince/weatherProvince.js
+// 查询能查询的地区、省份数据
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    result: '',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.request({
+      url: 'http://v.juhe.cn/historyWeather/province',
+      data: {
+        key: '0b218699fe61b0e3c79bb4e782cd1420'
+      },
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          "result": res.data.result,
+        })
+        console.log(that.data.result);
+      },
+    })
+  },
+   /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/test/test.js
@@ -0,0 +1,15 @@
+// pages/test/test.js
+Page({
+  data: {
+    imgUrls: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552335708359&di=2d9fc7c6fe72c537814f5049295e3a2b&imgtype=0&src=http%3A%2F%2Fimg4q.duitang.com%2Fuploads%2Fitem%2F201407%2F18%2F20140718194455_3mssA.jpeg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552335708358&di=957e3019cb1f4e16340450afe477c45b&imgtype=0&src=http%3A%2F%2Fcyf-img.bdstatic.com%2Fimg_5974b36f689fa_cf966ffdc8f1b9ecf84f7ae443075a05.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552335708357&di=24aa19280cb8a7c8d060fb229ae88ca1&imgtype=0&src=http%3A%2F%2Fi3.17173.itc.cn%2F2015%2Flol%2Fzt%2Fhuati%2F2015040201%2F5.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
+    circular:true
+  },
+})
